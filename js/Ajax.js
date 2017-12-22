c2b9e14d403bb57412a1ea560f7b6ba0f584e416
BramMoons <bram.moons@student.ehb.be>
2017-12-22 13:35:52 +0100

    BramV0.0.1

--- js/Ajax.js
+++ js/Ajax.js
@@ -1,54 +1,23 @@
 $(document).ready(){
     
     //start
-    $("Start").click(function(){ //Voor naam en email in de database te plaatsen
+    $("#Start").click(function(){ //Voor naam en email in de database te plaatsen
         $.ajax({
             method: "POST",
             url: '//dt5.ehb.be:3306/IP1718007',
             datatype: 'json',
             data: JSON.stringify(maakJsonVanInlogGegevens()),
             contentType: "application/json: charset=utf-8",
-            success: function (data, status){
-                updateDatabase();
-            }
         })
     })
     
-   function updateDatabase() { //Voor score bord
-
-        resetAfspraakDetails();
+    function scorebord(){
         $.ajax({
             method: "GET",
-            url: '//dt5.ehb.be:3306/IP1718007',
+            url : '//dt5.ehb.be:3306/IP1718007',
             dataType: 'json',
-            success: function (data, status) {
-                //Alle elementen in de select wissen (maakt de lijst leeg)
-                $("#lstAfspraken").empty();
-
-                $.each(data, function (huidigeIndex, huidigeAfspraak) {
-
-                    //Alle nieuwe keuzes in de lijst toevoegen => option elementen toevoegen
-                    //$("#lstAfspraken").append($("<option></option>").attr("value",huidigeAfspraak.afspraakId).text(huidigeAfspraak.naam));
-                    $("#lstGegevens")
-                            .append($("<a class='list-group-item afspraaktItem' href='#'></a>")
-                                    .attr("afspraakId", huidigeAfspraak.afspraakId)
-                                    .append($("<h4 class='list-group-item-heading'>")
-                                            .text(huidigeAfspraak.naam))
-                                    .append($("<p class='list-group-item-text'></p>")
-                                            .text(huidigeAfspraak.beschrijving))
-                                    .click(function (e) {
-                                        e.preventDefault();
-                                        //Hier schrijven we hetgene dat er moet gebeuren indien je klikt op een afspraak in het overzicht
-                                        $(this).addClass('active').siblings().removeClass('active');
-                                        geefAfspraakDetailsWeer(huidigeAfspraak.afspraakId);
-                                    }));
-                });
-
-            },
-            error: function (status) {
-                alert("Er is iets fout gegaan bij het ophalen van de afspraken");
-            }
-        });
+            
+        })
     }
     
      function maakJsonVanInlogGegevens() {
