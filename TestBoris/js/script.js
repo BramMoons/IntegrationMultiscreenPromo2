8d0b09aeaa04ba8e2ac7333fe84b72a9d0f1d7de
Boris Ysenbaert <boris.ysenbaert@student.ehb.be>
2018-01-09 13:58:55 +0100

    Javascript

---
+++ TestBoris/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  var punten = 0;
+
+  randomMonster();
+
+  $("#monster").click(function () {
+    $("#monster").offset({
+      top: Math.random() * 800,
+      left: Math.random() * 1300
+    });
+    punten = punten + 1;
+    $("#toonPunten").text(punten);
+  });
+
+});
+
+function randomMonster() {
+
+  $("#spel").html("<img id=monster src='images/vleermuis.png'>");
+
+  var x = (Math.floor((Math.random() * 50) + 1) + "%");
+  var y = (Math.floor((Math.random() * 85) + 1) + "%");
+
+  $("#monster").css({
+    "margin-top": x,
+    "margin-left": y
+  });
+}
