b8866fd9f287376e0cc7da025ea5b1c7b3c0069a
Boris Ysenbaert <boris.ysenbaert@student.ehb.be>
2018-01-09 16:33:14 +0100

    Game over

--- TestBoris/js/script.js
+++ TestBoris/js/script.js
@@ -1,30 +1,39 @@
 $(document).ready(function () {
   var punten = 0;
+  var leven = 5;
 
   randomMonster();
 
-  $("#monster").click(function () {
-
-    var x = Math.floor((Math.random() * 80) + 1) + "%";
-    var y = Math.floor((Math.random() * 80) + 1) + "%";
-
-    $("#monster").css({
-      "margin-top": y,
-      "margin-left": x
-
-    });
-    punten = punten + 1;
-    $("#toonPunten").text(punten);
+  $("body").on("click", function (e) {
+    leven = leven - 1;
+    $("#leven").text(leven);
+    if (leven === 0) {
+      window.location.href = "../pages/gameOver.html";
+    }
   });
 
+  $("body").on("click",
+    "#monster",
+    function (e) {
+      var x = Math.floor((Math.random() * 85) + 1) + "%";
+      var y = Math.floor((Math.random() * 50) + 1) + "%";
+
+      $("#monster").css({
+        "margin-top": y,
+        "margin-left": x
+      });
+
+      punten = punten + 1;
+      $("#toonPunten").text(punten);
+    });
 });
 
 function randomMonster() {
 
   $("#spel").html("<img id=monster src='../images/vleermuis.png'>");
 
-  var x = Math.floor((Math.random() * 80) + 1) + "%";
-  var y = Math.floor((Math.random() * 80) + 1) + "%";
+  var x = Math.floor((Math.random() * 85) + 1) + "%";
+  var y = Math.floor((Math.random() * 50) + 1) + "%";
 
 
   $("#monster").css({
