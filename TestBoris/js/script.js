303c6354e510d9833ad979c489d19384d432cb84
Boris Ysenbaert <boris.ysenbaert@student.ehb.be>
2018-01-10 10:37:02 +0100

    css levens

--- TestBoris/js/script.js
+++ TestBoris/js/script.js
@@ -5,30 +5,48 @@ $(document).ready(function () {
 
   randomMonster();
 
-  $("#bodySpel").on("click", function (e) {
-    leven = leven - 1;
-    $("#leven").text(leven);
-    if (leven === 0) {
-      window.location.href = "../pages/gameOver.html";
-    }
-  });
+  if (body.mouseDown())
+    console.log('jeeuj');
+}
 
-  $("#monster").on("click",
-    function (e) {
-      punt.play();
-      var x = Math.floor((Math.random() * 85) + 1) + "%";
-      var y = Math.floor((Math.random() * 50) + 1) + "%";
+//Click naast monster ----------------------------------------------------------------
 
-      $("#monster").css({
-        "margin-top": y,
-        "margin-left": x
-      });
+$("#bodySpel").on("click", function (e) {
+  leven = leven - 1;
+  $("#leven").text(leven);
+  if (leven === 0) {
+    window.location.href = "../pages/gameOver.html";
+  }
 
-      leven = leven + 1;
-      punten = punten + 1;
-      $("#toonPunten").text(punten);
-    });
+  $("li").last().remove();
+
 });
+
+//Click Monster -----------------------------------------------------------------
+
+document.body.img.onmousedown = (function () {
+  console.log('jeeuj');
+})
+
+$("#monster").on("click",
+  function (e) {
+    punt.play();
+    var x = Math.floor((Math.random() * 85) + 1) + "%";
+    var y = Math.floor((Math.random() * 50) + 1) + "%";
+
+    $("#monster").css({
+      "margin-top": y,
+      "margin-left": x
+    });
+
+    // Er wordt zowel op de body als de image geklikt, waardoor het leven ook naar beneden gaat.
+    leven = leven + 1;
+    punten = punten + 1;
+    $("#toonPunten").text(punten);
+  });
+});
+
+// Monster -----------------------------------------------------------------
 
 function randomMonster() {
 
