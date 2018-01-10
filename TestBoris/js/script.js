0c3561f132c860cce0e59cc691229a1cc23491ca
Dieter Blockmans <dieter.blockmans@gmail.com>
2018-01-10 10:40:13 +0100

    Dieter

--- TestBoris/js/script.js
+++ TestBoris/js/script.js
@@ -1,63 +1,76 @@
 $(document).ready(function () {
-  var punten = 0;
-  var leven = 5;
-  var punt = document.getElementById('punt');
+    var punten = 0;
+    var leven = 5;
+    var punt = document.getElementById('punt');
 
-  randomMonster();
+    //monster afbeeldingen paths in variabelen steken.
+    var vleermuis = '../images/vleermuis.png';
+    var bigfoot = '../images/bigFoot.png';
+    var spin = '../images/spin.png';
+    var zombieSpook = '../images/zombieSpook.png';
+    //alle monsters in een array steken.
+    var monsters = [vleermuis, bigfoot, spin, zombieSpook];
 
-  if (body.mouseDown())
-    console.log('jeeuj');
+    eersteMonster();
+
+    if (body.mouseDown())
+        console.log('jeeuj');
 }
 
 //Click naast monster ----------------------------------------------------------------
 
 $("#bodySpel").on("click", function (e) {
-  leven = leven - 1;
-  $("#leven").text(leven);
-  if (leven === 0) {
-    window.location.href = "../pages/gameOver.html";
-  }
+    leven = leven - 1;
+    $("#leven").text(leven);
+    if (leven === 0) {
+        window.location.href = "../pages/gameOver.html";
+    }
 
-  $("li").last().remove();
+    $("li").last().remove();
 
 });
 
 //Click Monster -----------------------------------------------------------------
 
 document.body.img.onmousedown = (function () {
-  console.log('jeeuj');
+    console.log('jeeuj');
 })
 
 $("#monster").on("click",
-  function (e) {
-    punt.play();
-    var x = Math.floor((Math.random() * 85) + 1) + "%";
-    var y = Math.floor((Math.random() * 50) + 1) + "%";
+    function (e) {
+        punt.play();
+        var x = Math.floor((Math.random() * 85) + 1) + "%";
+        var y = Math.floor((Math.random() * 50) + 1) + "%";
 
-    $("#monster").css({
-      "margin-top": y,
-      "margin-left": x
+        //variabelen die een random monster kiest.
+        var randomMonster = monsters[Math.floor(Math.random() * monsters.length)];
+        //de source van het monster veranderen zodat er altijd een ander monster staat.
+        $("#monster").attr("src", randomMonster);
+
+        $("#monster").css({
+            "margin-top": y,
+            "margin-left": x
+        });
+
+        // Er wordt zowel op de body als de image geklikt, waardoor het leven ook naar beneden gaat.
+        leven = leven + 1;
+        punten = punten + 1;
+        $("#toonPunten").text(punten);
     });
-
-    // Er wordt zowel op de body als de image geklikt, waardoor het leven ook naar beneden gaat.
-    leven = leven + 1;
-    punten = punten + 1;
-    $("#toonPunten").text(punten);
-  });
 });
 
 // Monster -----------------------------------------------------------------
 
-function randomMonster() {
+function eersteMonster() {
 
-  $("#spel").html("<img id=monster src='../images/vleermuis.png'>");
+    $("#spel").html("<img id=monster src='../images/vleermuis.png'>");
 
-  var x = Math.floor((Math.random() * 85) + 1) + "%";
-  var y = Math.floor((Math.random() * 50) + 1) + "%";
+    var x = Math.floor((Math.random() * 85) + 1) + "%";
+    var y = Math.floor((Math.random() * 50) + 1) + "%";
 
 
-  $("#monster").css({
-    "margin-top": y,
-    "margin-left": x
-  });
+    $("#monster").css({
+        "margin-top": y,
+        "margin-left": x
+    });
 }
